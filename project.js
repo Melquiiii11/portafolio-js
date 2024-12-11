addd1983e1e867fea4a9913edb16e3d833ff736a
Melquiiii11 <angelmelquisedec88@gmail.com>
2024-12-11 20:32:10 -0300

    cambios ultimos para terminar el projecto

--- project.js
+++ project.js
@@ -1,27 +1,36 @@
-const projects = [
-    {
-        src: "https://github.com/tecnologias-web/catalogo-cursos-frontend/blob/master/css/index.css",
-        title: "Generator Password",
-        description: "Generado de password" 
-    },
-];
+const jsonUrl = 'project.json'; // Asegúrate de que esta ruta sea correcta
+const container = document.querySelector('.divproject');
 
-function addProject(projects){
-    let html = '';
-    projects.ForEach(function(project){
-        html +=  `
-        <div class="item-project">
-          <img src="${project.src}" />
-          <h3>${project.title}</h3>
-          <p>${project.description}€</p>
-        </div>
-      `;
-    });
+async function loadProjects() {
+    try {
+        const response = await fetch(jsonUrl);
+        const projects = await response.json();
 
-$('.item-project').html(html);
+        // Limpiar contenido previo
+        container.innerHTML = '';
 
+        // Generar dinámicamente cada proyecto
+        projects.forEach(project => {
+            const projectDiv = document.createElement('div');
+            projectDiv.classList.add('item-project');
 
+            projectDiv.innerHTML = `
+                <a href="${project.imgLink}" target="_blank">
+                    <img src="${project.img}" alt="Project image">
+                </a>
+                <h3>${project.title}</h3>
+                <p>${project.description}</p>
+                <a href="${project.github}" class="link" target="_blank">
+                    <img src="./img/github.png" alt="GitHub link">
+                </a>
+            `;
+
+            // Añadir el proyecto al contenedor
+            container.appendChild(projectDiv);
+        });
+    } catch (error) {
+        console.error('Error loading projects:', error);
+    }
 }
-$(function(){
-    addProject(projects);
-});
+
+loadProjects();
